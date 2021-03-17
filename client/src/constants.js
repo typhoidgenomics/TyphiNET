11d6d8b91ca24d99ebae109ffbaa9e7ef364e646
lcerdeira <lcerdeira@gmail.com>
2021-03-17 14:08:30 -0300

    fetch re

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,2 +1,2 @@
-//export const API_ENDPOINT = "http://localhost:8080/api/";
-export const API_ENDPOINT = "https://typhinet.herokuapp.com/api/";
+export const API_ENDPOINT = "http://localhost:8080/api/";
+// export const API_ENDPOINT = "https://typhinet.herokuapp.com/api/";
