7a8a2e5619acb6a3a25ac6dfa97903e8d8c5fd78
Kat Holt <drkatholt@gmail.com>
2023-06-12 14:27:43 +0100

    update about

--- client/src/components/About/About.js
+++ client/src/components/About/About.js
@@ -20,18 +20,18 @@ export const About = () => {
 
         <div className={classes.paragraph}>
           AMR determinants identified in the genome assemblies using Pathogenwatch are used to define drug resistance
-          variables as follows.Multidrug resistance(MDR): resistance determinants for chloramphenicol (<i>catA1</i> or{' '}
+          variables as follows. Multidrug resistance (MDR): resistance determinants for chloramphenicol (<i>catA1</i> or{' '}
           <i>cmlA</i>), ampicillin (<i>bla</i>TEM-1D, <i>bla</i>OXA-7), and co-trimoxazole (at least one <i>dfrA</i>{' '}
-          gene and at least one <i>sul</i> gene). Ciprofloxacin non-susceptible (CipI/R): one or more of the quinolone
-          resistance determining region(QRDR) mutations at <i>gyrA</i>-83, <i>gyrA</i>
-          -87, <i>parC</i>-80, <i>parC</i>-84, <i>gyrB</i>-464 or presence of a plasmid - mediated quinolone
-          resistance(PMQR) gene(<i>qnrB</i>, <i>qnrD</i> , <i>qnrS</i>). Ciprofloxacin resistant (CipR): QRDR triple
-          mutant (<i>gyrA</i>-83 and <i>gyrA</i>-87, together with either <i>parC</i>-80 or <i>parC</i>-84), or plasmid
-          - mediated quinolone resistance(PMQR) together with <i>gyrA</i>-83, <i>gyrA</i>
-          -87 and/or <i>gyrB</i>-464. Third - generation cephalosporin resistance (3 GCR): presence of an extended -
-          spectrum beta-lactamase(ESBL) (<i>bla</i>CTX-M-12, <i>bla</i>CTX-M-15, <i>bla</i>CTX-M-23, <i>bla</i>CTX-M-55,{' '}
-          <i>bla</i>SHV-12) or <i>ampC</i> gene.Extreme drug resistance(XDR): MDR plus CipR plus 3 GCR. Azithromycin
-          resistance(AziR): mutation at <i>acrB</i>-717 and/or gene <i>ereA</i>. See{' '}
+          gene and at least one <i>sul</i> gene). Ciprofloxacin non-susceptible (CipNS): one or more of the quinolone
+          resistance determining region (QRDR) mutations at <i>gyrA</i>-83, <i>gyrA</i>
+          -87, <i>parC</i>-80, <i>parC</i>-84, <i>gyrB</i>-464 or presence of a plasmid-mediated quinolone
+          resistance (PMQR) gene (<i>qnrB</i>, <i>qnrD</i>, <i>qnrS</i>). Ciprofloxacin resistant (CipR): QRDR triple
+          mutant (<i>gyrA</i>-83 and <i>gyrA</i>-87, together with either <i>parC</i>-80 or <i>parC</i>-84), or plasmid-mediated 
+          quinolone resistance (PMQR) together with <i>gyrA</i>-83, <i>gyrA</i>-87 and/or <i>gyrB</i>-464. Third-generation 
+          cephalosporin resistance (3GCR): presence of an extended-spectrum beta-lactamase(ESBL) (<i>bla</i>CTX-M-12, 
+          <i>bla</i>CTX-M-15, <i>bla</i>CTX-M-23, <i>bla</i>CTX-M-55,{' '} <i>bla</i>SHV-12) or <i>ampC</i> gene.
+          Extreme drug resistance (XDR): MDR plus CipR plus 3GCR. Azithromycin resistance (AziR): mutation at <i>acrB</i>-717. 
+          See{' '}
           <a href="https://www.nature.com/articles/s41467-021-23091-2" target="_blank" rel="noreferrer">
             Argimon et al, 2021
           </a>{' '}
@@ -43,20 +43,22 @@ export const About = () => {
           <a href="https://pathogen.watch/" target="_blank" rel="noreferrer">
             Pathogenwatch
           </a>
-          , and filtered to include only genomes from unbiased sampling frames (e.g. routine or project - based enteric
-          fever surveillance, as opposed to AMR - focused sampling), based on curation by the{' '}
+          , and filtered to include only genomes from unbiased sampling frames (e.g. routine or project-based enteric
+          fever surveillance, as opposed to AMR-focused sampling), based on curation by the{' '}
           <a href="https://www.typhoidgenomics.org/" target="_blank" rel="noreferrer">
             Global Typhoid Genomics Consortium
+          </a> 
+          {' '}as described in{' '}
+          < a href="https://www.medrxiv.org/content/10.1101/2022.12.28.22283969v1" target="_blank" rel="noreferrer">
+          	this paper
           </a>
-          . The database can be downloaded using the button below.
+          . The database can be downloaded using the button above.
           <p>
             <b>Documentation:</b> Full documentation for the dashboard is available{' '}
             <a href="https://github.com/zadyson/TyphiNET/wiki" target="_blank" rel="noreferrer">
               here
             </a>
-            .
-          </p>
-          Information on genotype definitions can be found in{' '}
+            . Information on genotype definitions can be found in{' '}
           <a
             href="https://academic.oup.com/jid/article/224/Supplement_7/S775/6358992?login=true"
             target="_blank"
@@ -65,6 +67,7 @@ export const About = () => {
             Dyson & Holt, 2021
           </a>
           .
+           </p>
         </div>
 
         <div className={classes.paragraph}>
@@ -76,7 +79,7 @@ export const About = () => {
           <a href="https://scholar.google.com.au/citations?hl=en&user=O2dcz5MAAAAJ" target="_blank" rel="noreferrer">
             Dr Louise Cerdeira
           </a>
-          &amp;{' '}
+          {' '} and {' '}
           <a href="https://holtlab.net/" target="_blank" rel="noreferrer">
             Prof Kat Holt
           </a>
@@ -86,7 +89,7 @@ export const About = () => {
           <a href="https://www.lshtm.ac.uk/" target="_blank" rel="noreferrer">
             London School of Hygiene and Tropical Medicine
           </a>{' '}
-          &amp;{' '}
+          and {' '}
           <a href="https://www.monash.edu/" target="_blank" rel="noreferrer">
             Monash University
           </a>
