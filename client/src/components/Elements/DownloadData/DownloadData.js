53f9b79d46b13b2b50f11f44c77412415b4f47d8
vlshtm <vandanalshtm@gmail.com>
2023-08-17 17:34:58 +0100

    cip_pred_pheno to Cip in download csv

--- client/src/components/Elements/DownloadData/DownloadData.js
+++ client/src/components/Elements/DownloadData/DownloadData.js
@@ -101,6 +101,11 @@ export const DownloadData = () => {
           let line = csv[index].split(',');
           lines.push(line);
         }
+        lines[0].forEach((curr, index) => {
+          if (curr === 'cip_pred_pheno') {
+            lines[0][index] = 'Cip';
+          }
+        });
 
         for (let index = 0; index < columnsToRemove.length; index++) {
           let currentIndex = lines[0].indexOf(columnsToRemove[index]);
