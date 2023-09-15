553216dd2a7cc67c962fa86b1a506b02d114fc37
Louise Cerdeira <lcerdeira@gmail.com>
2023-09-15 17:30:44 -0300

    fix and update branch

--- client/src/util/mapLegends.js
+++ client/src/util/mapLegends.js
@@ -3,7 +3,7 @@ export const mapLegends = [
   { value: 'MDR', label: 'Multidrug resistant (MDR)' },
   { value: 'XDR', label: 'Extensively drug resistant (XDR)' },
   { value: 'AzithR', label: 'Azithromycin resistant' },
-  { value: 'CipNS', label: 'Ciprofloxacin nonsusceptible (CipNS)' },
+  { value: 'CipNS', label: 'Ciprofloxacin non-susceptible (CipNS)' },
   { value: 'CipR', label: 'Ciprofloxacin resistant (CipR)' },
   // { value: 'Sensitive to all drugs', label: 'Sensitive to all drugs' },
   { value: 'Susceptible to all drugs', label: 'Susceptible to all drugs' },
