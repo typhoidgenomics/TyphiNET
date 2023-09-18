988627cf6af7eed4fde8e9690d322f0f4deaac19
vlshtm <vandanalshtm@gmail.com>
2023-09-18 12:40:49 +0100

    Update Plot Heading alignment in Report to "Center"

--- client/src/components/Elements/Graphs/Graphs.js
+++ client/src/components/Elements/Graphs/Graphs.js
@@ -124,18 +124,18 @@ export const Graphs = () => {
      
       ctx.font = 'bold 18px Montserrat';
       ctx.fillStyle = 'black';
-      ctx.textAlign = 'right';
-      ctx.fillText(card.title, (canvas.width+graphImg.width)/2 ,50);
+      ctx.textAlign = 'center';
+      ctx.fillText(card.title, canvas.width / 2, 50);
 
       ctx.font = '12px Montserrat';
-      ctx.fillText(card.description.join(' / '), (canvas.width+graphImg.width)/2, 72);
+      ctx.fillText(card.description.join(' / '), canvas.width / 2, 72);
 
       ctx.font = '14px Montserrat';
-      // ctx.fillText(`Organism: ${globalOverviewLabel.fullLabel}`, (canvas.width+graphImg.width)/2, 110);
-      ctx.fillText(`Dataset: ${dataset}`, (canvas.width+graphImg.width)/2, 110);
-      ctx.fillText(`Time period: ${actualTimeInitial} to ${actualTimeFinal}`, (canvas.width+graphImg.width)/2, 132);
-      ctx.fillText(`Country: ${actualCountry}`, (canvas.width+graphImg.width)/2, 154);
-      if (card.id === 'RDWG') ctx.fillText(`Drug Class: ${determinantsGraphDrugClass}`, (canvas.width+graphImg.width)/2, 176);
+      // ctx.fillText(`Organism: ${globalOverviewLabel.fullLabel}`, canvas.width / 2, 110);
+      ctx.fillText(`Dataset: ${dataset}`, canvas.width / 2, 110);
+      ctx.fillText(`Time period: ${actualTimeInitial} to ${actualTimeFinal}`, canvas.width / 2, 132);
+      ctx.fillText(`Country: ${actualCountry}`, canvas.width / 2, 154);
+      if (card.id === 'RDWG') ctx.fillText(`Drug Class: ${determinantsGraphDrugClass}`, canvas.width / 2, 176);
 
       ctx.fillStyle = 'white';
       ctx.textAlign = 'start';
