d57ebcfca4b22b1f4651d40eb1bd69ac1991a376
Louise Cerdeira <lcerdeira@gmail.com>
2023-09-20 15:19:26 -0300

    cip_pred_pheno changed fixed variable call

--- client/src/components/Elements/Map/Map.js
+++ client/src/components/Elements/Map/Map.js
@@ -37,6 +37,7 @@ export const Map = () => {
   const mapView = useAppSelector((state) => state.map.mapView);
   const tooltipContent = useAppSelector((state) => state.map.tooltipContent);
   const globalOverviewLabel = useAppSelector((state) => state.dashboard.globalOverviewLabel);
+  const frequenciesGraphSelectedGenotypes = useAppSelector((state) => state.graph.frequenciesGraphSelectedGenotypes);
   const customDropdownMapView = useAppSelector((state) => state.graph.customDropdownMapView);
   const ifCustom = useAppSelector((state) => state.map.ifCustom);
 
