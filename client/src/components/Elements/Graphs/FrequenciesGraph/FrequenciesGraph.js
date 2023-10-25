dff9ca766f155fac3d4e697348f37a041f8ddc18
vlshtm <vandanalshtm@gmail.com>
2023-10-25 15:30:16 +0100

    update NaN% of susceptible in dropdowns

--- client/src/components/Elements/Graphs/FrequenciesGraph/FrequenciesGraph.js
+++ client/src/components/Elements/Graphs/FrequenciesGraph/FrequenciesGraph.js
@@ -50,8 +50,8 @@ export const FrequenciesGraph = () => {
 
   function getSelectGenotypeLabel(genotype) {
     const percentage = Number(((genotype.Susceptible / genotype.totalCount) * 100).toFixed(2));
+    return `${genotype.name} (total N=${genotype.totalCount===0 ? 0:`${genotype.totalCount},${percentage}% Susceptible`})`;
 
-    return `${genotype.name} (total N=${genotype.totalCount}, ${percentage}% Susceptible)`;
   }
 
   function getDomain() {
