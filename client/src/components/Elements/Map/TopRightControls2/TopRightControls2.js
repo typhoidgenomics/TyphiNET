2cbc8666bd9b683fc771b497b40abff5d14ed297
vlshtm <vandanalshtm@gmail.com>
2023-11-25 18:31:28 +0530

    Update Map view component

--- client/src/components/Elements/Map/TopRightControls2/TopRightControls2.js
+++ client/src/components/Elements/Map/TopRightControls2/TopRightControls2.js
@@ -28,53 +28,12 @@ export const TopRightControls2 = () => {
     setSelectedValues(newValue);
   };
 
-  // useEffect(() => {
-  //   setCurrentTooltip(null);
-  // }, [genotypesDrugsData, customDropdownMapView]);
   function getSelectGenotypeLabel(genotype) {
     const matchingGenotype = genotypesDrugsData.find(g => g.name === genotype);
     const totalCount = matchingGenotype?.totalCount ?? 0;
     const susceptiblePercentage = (matchingGenotype?.Susceptible / totalCount || 0) * 100;
     return `${genotype} (total N=${totalCount}, ${susceptiblePercentage.toFixed(2)}% Susceptible)`;
 }
-
-  console.log("customDropdownMapView",customDropdownMapView )
-
-  function handleChangeSelectedGenotypes({ event = null, all = false }) {
-    if (all) {
-      dispatch(setCustomDropdownMapView([]));
-      setCurrentTooltip(null);
-      return;
-    }
-
-    const {
-      target: { value }
-    } = event;
-
-    if (customDropdownMapView.length === 10 && value.length > 10) {
-      return;
-    }
-
-    if (value.length === 0) {
-      setCurrentTooltip(null);
-    }
-    dispatch(setCustomDropdownMapView(value));
-  }
-
- function setSearchValue(event){
-  event.preventDefault()
-  setSearchValue2(event.target.value)
- }
-
- const getDynamicOptionLabel = (option, customDropdownMapView) => {
-  if (customDropdownMapView.length <= 10) {
-    // Implement your dynamic label logic here
-    return ;
-  }
-  // Fallback to the default label logic
-  return option;
-};
-
 
 const filteredData = genotypesDrugsData2
     .filter((genotype) => genotype.name.includes(searchValue2.toLowerCase()) || genotype.name.includes(searchValue2.toUpperCase()))
@@ -101,8 +60,8 @@ const filteredData = genotypesDrugsData2
             limitTags={1}
             id="tags-standard"
             options={filteredData.map((data) => data.name) }
-            freeSolo={customDropdownMapView.length >= 3 ? false : true}
-            getOptionDisabled={(options) => (customDropdownMapView.length >=3 ? true : false)}
+            freeSolo={customDropdownMapView.length >= 10 ? false : true}
+            getOptionDisabled={(options) => (customDropdownMapView.length >= 10 ? true : false)}
             value={selectedValues}
             disableCloseOnSelect
             onChange={handleAutocompleteChange}
@@ -124,66 +83,6 @@ const filteredData = genotypesDrugsData2
               />
             )}
           />
-          {/* <Autocomplete
-          multiple
-          id="genotypes-autocomplete"
-          options={filteredData.map((data) => data.name)}
-          // value={customDropdownMapView}
-          defaultValue={[customDropdownMapView[0]]}
-          onChange={(_, newValue) => handleChangeSelectedGenotypes({ event: { target: { value: newValue } } })}
-          disableCloseOnSelect
-          getOptionLabel={(option) => option}
-          isOptionEqualToValue={(option, value) => option.name === value.name}
-          renderOption={(props, option, { selected }) => (
-            <MenuItem
-                key={option}
-                value={option}
-                sx={{ justifyContent: "space-between" }}
-                // {...props}
-              >
-                <li {...props}>
-              <Checkbox checked={selected} />
-              <ListItemText primary={getSelectGenotypeLabel(option)} />
-            </li>
-              </MenuItem>
-            
-          )}
-          renderInput={(params) => (
-            <TextField
-              {...params}
-              size="small"
-              autoFocus
-              placeholder="Type to search..."
-              // label="Search genotype"
-              variant="standard"
-              // InputProps={{
-              //   ...params.InputProps,
-              //   startAdornment: (
-              //     <InputAdornment position="start">
-              //       <SearchIcon />
-              //     </InputAdornment>
-              //   ),
-                // endAdornment: (
-                //   <InputAdornment position="end">
-                //     <Button
-                //       variant="outlined"
-                //       className={classes.genotypesSelectButton}
-                //       onClick={() => handleChangeSelectedGenotypes({ all: true })}
-                //       disabled={customDropdownMapView.length === 0}
-                //       color="error"
-                //     >
-                //       Clear All
-                //     </Button>
-                //   </InputAdornment>
-                // ),
-              // }}
-              onChange={(e) => {
-                setSearchValue(e.target.value);
-                // setSearchValue2(""); // Assuming setSearchValue2 is a function to update the second search value
-              }}
-              onKeyDown={(e) => e.stopPropagation()}
-            />
-          )} */}
           </FormControl>
         </CardContent>
      </Card>
