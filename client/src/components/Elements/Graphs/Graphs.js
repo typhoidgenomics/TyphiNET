22ed08a082694c81715381da57fae12f4cdfc718
vlshtm <vandanalshtm@gmail.com>
2024-02-12 16:57:44 +0000

    Update plot DRT if no drug is selected

--- client/src/components/Elements/Graphs/Graphs.js
+++ client/src/components/Elements/Graphs/Graphs.js
@@ -79,6 +79,12 @@ export const Graphs = () => {
   async function handleClickDownload(event, card) {
     event.stopPropagation();
     handleLoading(card.id, true);
+    if(drugResistanceGraphView.length === 0){
+      handleLoading(card.id, false);
+      return(
+        alert("No drugs/classes selected to download")
+      )
+    }
 
     try {
       const canvas = document.createElement('canvas');
@@ -96,8 +102,17 @@ export const Graphs = () => {
         drugClassesFactor,
         genotypesFactor;
 
-      if (['RFWG', 'DRT'].includes(card.id)) {
+      if (['RFWG'].includes(card.id)) {
         heightFactor = 250;
+      }
+      if (['DRT'].includes(card.id)  && drugResistanceGraphView.length!==0 ) {
+        heightFactor = 250;
+      } else{
+        (
+          <Tooltip title="Extensively drug resistant(XDR): MDR plus CipR plus ESBL." placement="top">
+            <span>XDR</span>
+            </Tooltip>
+        );
       }
       if (card.id === 'RDWG') {
         drugClassesBars = colorForDrugClasses[determinantsGraphDrugClass];
