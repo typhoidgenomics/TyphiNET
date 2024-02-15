c80ffbdfa26ded274df936b48eb2277823e4ecb1
Louise Cerdeira <lcerdeira@gmail.com>
2024-02-15 18:51:58 +0000

    update service sintax

--- client/src/registerServiceWorker.js
+++ client/src/registerServiceWorker.js
@@ -1,47 +1,3 @@
-// In production, we register a service worker to serve assets from local cache.
-
-// This lets the app load faster on subsequent visits in production, and gives
-// it offline capabilities. However, it also means that developers (and users)
-// will only see deployed updates on the "N+1" visit to a page, since previously
-// cached resources are updated in the background.
-
-// To learn more about the benefits of this model, read https://goo.gl/KwvDNy.
-// This link also includes instructions on opting out of this behavior.
-
-// const isLocalhost = Boolean(
-//   window.location.hostname === "localhost" ||
-//     // [::1] is the IPv6 localhost address.
-//     window.location.hostname === "[::1]" ||
-//     // 127.0.0.1/8 is considered localhost for IPv4.
-//     window.location.hostname.match(
-//       /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
-//     )
-// );
-
-export default function register() {
-  if (!process.env.PUBLIC_URL && process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
-    // The URL constructor is available in all browsers that support SW.
-    // const publicUrl = new URL(process.env.PUBLIC_URL, window.location);
-    // if (publicUrl.origin !== window.location.origin) {
-    //   // Our service worker won't work if PUBLIC_URL is on a different origin
-    //   // from what our page is served on. This might happen if a CDN is used to
-    //   // serve assets; see https://github.com/facebookincubator/create-react-app/issues/2374
-    //   return;
-    // }
-    // window.addEventListener("load", () => {
-    //   const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
-
-    //   if (!isLocalhost) {
-    //     // Is not local host. Just register service worker
-    //     registerValidSW(swUrl);
-    //   } else {
-    //     // This is running on localhost. Lets check if a service worker still exists or not.
-    //     checkValidServiceWorker(swUrl);
-    //   }
-    // });
-  }
-// }
-
 function registerValidSW(swUrl) {
   navigator.serviceWorker
     .register(swUrl)
@@ -70,7 +26,6 @@ function registerValidSW(swUrl) {
       console.error("Error during service worker registration:", error);
     });
 }
-
 function checkValidServiceWorker(swUrl) {
   // Check if the service worker can be found. If it can't reload the page.
   fetch(swUrl)
@@ -97,7 +52,10 @@ function checkValidServiceWorker(swUrl) {
       );
     });
 }
-
+export default function register() {
+  if (!process.env.PUBLIC_URL && process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
+  }
+}
 export function unregister() {
   if ("serviceWorker" in navigator) {
     navigator.serviceWorker.ready.then(registration => {
